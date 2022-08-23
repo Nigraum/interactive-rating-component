097c5156481e04ca7a19ffcf349c4336ecf2be05
Nigraum <yan.corintima@hotmail.com>
2022-08-23 16:40:39 -0300

    Create consts

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,8 @@
 const mainContainer = document.querySelector(".container")
+const thanksContainer = document.querySelector(".thank-you")
+const submitButton = document.getElementById("submit-rating")
+const rateAgain = document.getElementById("rate-again")
+const ratings = document.querySelectorAll(".btn")
+const actualRating = document.getElementById("rating")
+
+
